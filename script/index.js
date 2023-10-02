3aff159fea55be7d3fad24dd004ae3b2896fc584
Алик <nar.080@mail.ru>
2023-10-02 17:04:09 +0300

    add

--- script/index.js
+++ script/index.js
@@ -2,12 +2,16 @@ const modal = document.querySelector("#myModal");
 const btn = document.querySelector("#myBtn")
 const span = document.querySelector(".btn-close")
 const close = document.querySelector('.btn-secondary')
-
+const modalContents = document.querySelector('.modal-contents')
 btn.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "block"
 
+
 });
+btn.addEventListener('click',(evt)=>{
+    modalContents.style.left = '10%';
+})
 close.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "none"
