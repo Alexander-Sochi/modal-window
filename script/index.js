f72c3ee2c12da4afe7cb7adc806b7c40bbc2ba4c
Алик <nar.080@mail.ru>
2023-10-02 18:22:58 +0300

    add

--- script/index.js
+++ script/index.js
@@ -6,25 +6,30 @@ const modalContents = document.querySelector('.modal-contents')
 btn.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "block"
-
-
 });
-btn.addEventListener('click',(evt)=>{
-    modalContents.style.left = '10%';
-})
+function btnLeft (evt){
+    evt.preventDefault()
+    setTimeout(function (){
+        modalContents.style.left = '10%';
+    },500)
+}
+btn.addEventListener('click',btnLeft)
 close.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "none"
+    document.removeEventListener('click',btnLeft)
 });
 
 span.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "none"
+    document.removeEventListener('click',btnLeft)
 });
 
 window.addEventListener('click',(evt)=>{
     evt.preventDefault()
     if(evt.target == modal){
         modal.style.display = "none"
+        document.removeEventListener('click',btnLeft)
     }
 })
