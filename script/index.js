731d5822d4021bbf1ee404312dba67c2eef76da6
Алик <nar.080@mail.ru>
2023-10-03 19:58:23 +0300

    refactor naming

--- script/index.js
+++ script/index.js
@@ -1,35 +1,50 @@
-const modal = document.querySelector("#myModal");
+const modal = document.querySelector("#PayReqModal ");
 const btn = document.querySelector("#myBtn")
 const span = document.querySelector(".btn-close")
 const close = document.querySelector('.btn-secondary')
 const modalContents = document.querySelector('.modal-contents')
+
+
 btn.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "block"
+    modalContents.style.left = '102%';
 });
 function btnLeft (evt){
     evt.preventDefault()
     setTimeout(function (){
         modalContents.style.left = '10%';
-    },500)
+    },500)  
 }
 btn.addEventListener('click',btnLeft)
+
 close.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "none"
-    document.removeEventListener('click',btnLeft)
+    const modalNone = modal.style.display = "none"
+    modalContents.style.left = '102%';
+
+   
 });
 
 span.addEventListener('click',(evt)=>{
     evt.preventDefault()
     modal.style.display = "none"
-    document.removeEventListener('click',btnLeft)
+    const modalNone = modal.style.display = "none"
+    modalContents.style.left = '102%';
+   
 });
 
 window.addEventListener('click',(evt)=>{
     evt.preventDefault()
     if(evt.target == modal){
         modal.style.display = "none"
-        document.removeEventListener('click',btnLeft)
-    }
+        modalContents.style.left = '102%';
+    } 
+})
+document.addEventListener('keydown',(evt)=>{
+    evt.preventDefault()
+    if (evt.key === 'Escape') {
+        modal.style.display = "none"
+      }
 })
